44995681306384a2d101314eeba6c203c8b79f94
Akmal <akmalthemalong@gmail.com>
2023-05-19 18:22:11 +0800

    change (code): mongoose object modeling implementation

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-const { getBookmark, getCategory, addCategory, addLink, deleteCategory, deleteLink, editCategory } = require('./utils/keyword')
+
+// DB Init
+require('./model/connect')
+const { Url, Category } = require('./model/bookmark')
+const {
+  findAll,
+  addCategory,
+  addUrl,
+  deleteUrl,
+  deleteCategory,
+  editCategory
+} = require('./utils/category')
+
 
 const app = express()
 const port = 3000
@@ -16,72 +28,74 @@ app.set('layout', 'layouts/main.ejs')
 // set static folder
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  const bookmark = getBookmark()
-  res.render('index', { bookmark })
+app.get('/', async (req, res) => {
+  res.render('index', { bookmark: await findAll() })
 })
 
 // add category
 app.get('/add', (req, res) => {
-  res.render('add')
+  res.render('add-category', { errorMessage: '' })
 })
 
-app.post('/add', (req, res) => {
-  addCategory(req.body)
-  res.redirect('/')
-})
+app.post('/add', async (req, res) => {
+  const resp = await addCategory(req.body.category, req.body.url)
 
-// add link to the category
-app.get('/add/link/:category', (req, res) => {
-  const category = getCategory(req.params.category)
-  if (category) {
-    res.render('add-link', {
-      category: req.params.category
+  if (!resp) {
+    res.render('add-category', {
+      errorMessage: `${req.body.category} telah ada`
     })
   } else {
-    res.status(404).send('not found')
+    res.redirect('/')
   }
 })
 
-app.post('/add/link', (req, res) => {
-  addLink(req.body.category, req.body.links)
+// add link to the category
+app.get('/add/link/:id', async (req, res) => {
+  const document = await Category.findById(req.params.id, 'category')
+  res.render('add-link', { category: document.category, id: document.id })
+})
+
+app.post('/add/link', async (req, res) => {
+  await addUrl(req.body.id, req.body.links)
   res.redirect('/')
 })
 
 // delete category
-app.get('/delete/:category', (req, res) => {
-  deleteCategory(req.params.category)
+app.get('/delete/:id', (req, res) => {
+  deleteCategory(req.params.id)
   res.redirect('/')
 })
 
 // delete link from the category
-app.post('/delete/link', (req, res) => {
-  deleteLink(req.body.category, req.body.link)
+app.post('/delete/url', (req, res) => {
+  deleteUrl(req.body)
   res.redirect('/')
 })
 
 // show all link of category
-app.get('/manage/:category', (req, res) => {
-  const category = getCategory(req.params.category)
-  if (category) {
-    res.render('manage', { category })
+app.get('/manage/:id', async (req, res) => {
+  const document = await Category.findById(req.params.id)
+    .populate('urls')
+
+  if (document) {
+    res.render('manage', { document })
   } else {
     res.status(404).send('not found')
   }
 })
 
 // change category title
-app.get('/edit/:category', (req, res) => {
-  const category = getCategory(req.params.category)
-  if (category) {
-    res.render('edit', { category })
+app.get('/edit/:id', async (req, res) => {
+  const document = await Category.findById(req.params.id)
+  if (document) {
+    res.render('edit', { document })
   } else {
     res.status(404).send('keyword not found')
   }
 })
 
-app.post('/edit', (req, res) => {
-  editCategory(req.body.category, req.body.newCategory)
+app.post('/edit', async (req, res) => {
+  await editCategory(req.body.id, req.body.newCategory)
   res.redirect('/')
 })
 
